1804efb96e6245ac4e606ed8d51dcb4bf33de5e5
Bohdan Partyk <info@podebrady.org>
2019-02-24 17:49:24 +0100

    zadání 2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
 import '@babel/polyfill';
 import Vue from 'vue';
 
-global.Vue = Vue;
+global.app = new Vue({
+    el: '#app',
+    data: {
+        list: [],
+        inputText: null,
+        submittedText: null
+    },
+    methods: {
+        submitForm() {
+            if (this.inputText.length === 0) {
+                return;
+            }
+            this.submittedText = this.inputText;
+            this.list.unshift(this.inputText);
+            this.resetForm();
+        },
+        resetForm() {
+            this.inputText = null;
+        },
+        removeItem(index) {
+            this.submittedText = null;
+            this.list.splice(index, 1);
+        }
+    }
+});
