964c075d39cee4f472ed0b24f106457debb11bdb
Bohdan Partyk <info@podebrady.org>
2019-12-25 12:47:47 +0100

    progress bar

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <div class="progress mb-3">
+            <div class="progress-bar progress-bar-striped progress-bar-animated bg-success"
+                 role="progressbar"
+                 :style="{ width: value + '%' }"
+                 :aria-valuenow="value"
+                 aria-valuemin="0"
+                 aria-valuemax="100"
+                >
+                {{ value }}%
+            </div>
+        </div>
+        <div class="form-group mb-3">
+            <label for="formControlRange">Example Range input</label>
+            <input type="range" class="form-control-range" id="formControlRange" min="0" max="100" v-model.number="value">
+        </div>
+        <button v-on:click="raiseBar" class="btn btn-secondary mb-10">
+            Progress bar up
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            value: 20
+        };
+    },
+    methods: {
+        raiseBar() {
+            if (this.value < 100) {
+                this.value += 1;
+            } else {
+                this.value = 20;
+            }
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.progress {
+    .progress-bar {
+        font-weight: bold;
+    }
+}
+</style>
--- src/js/src/main.js
+++ src/js/src/main.js
@@ -3,12 +3,14 @@ import 'regenerator-runtime/runtime';
 import Vue from 'vue/dist/vue.esm';
 import Dude from '../../components/Dude';
 import Listgroupitem from '../../components/Listgroupitem';
+import ProgressBar from '../../components/ProgressBar';
 
 new Vue({
     el: '#app',
     components: {
         Dude,
-        Listgroupitem
+        Listgroupitem,
+        ProgressBar
     },
     data: {
         newDude: '',
